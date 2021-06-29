91de6c430b693807ec47f5b6dd3e1db55d009e8f
Zhihan Zhang <zhihanzhang@Zhihans-MacBook-Pro.local>
2021-06-29 12:40:17 +0800

    update interface for OSS

--- chatbots/utils/storage.go
+++ chatbots/utils/storage.go
@@ -2,18 +2,17 @@ package utils
 
 import (
 	"context"
-	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
 type StorageInterface interface {
-	Store(ctx context.Context, info []byte) error
-	Retrieve(ctx context.Context) ([]byte, error)
+	Store(ctx context.Context, pr, sha, filename string, info []byte) error
+	Retrieve(ctx context.Context, pr, sha, filename string) ([]byte, error)
 	GetBasePath() string
-	SetStoragePath(s string) string
-	BuildStoragePath(pr, sha string) (string, error)
+	List(ctx context.Context)([]meta, error)
 }
 
 type FileStorage struct {
@@ -21,15 +20,42 @@ type FileStorage struct {
 	FilePath string //
 }
 
-// define the location you want to store your files
-func (r *FileStorage) Store(ctx context.Context, data []byte) error {
-	err := ioutil.WriteFile(r.FilePath, data, 0666)
+// Store define the location you want to store your files
+func (r *FileStorage) Store(ctx context.Context, pr, sha, filename string, data []byte) error {
+	basePath, err := filepath.Abs(r.GetBasePath())
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(basePath); os.IsNotExist(err) {
+		err := os.Mkdir(basePath, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	PRPath := filepath.Join(basePath, pr)
+	if _, err := os.Stat(PRPath); os.IsNotExist(err) {
+		err := os.Mkdir(PRPath, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	SHAPath := filepath.Join(PRPath, sha)
+	if _, err := os.Stat(SHAPath); os.IsNotExist(err) {
+		err := os.Mkdir(SHAPath, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	filename = filepath.Join(SHAPath, filename)
+	err = ioutil.WriteFile(filename, data, 0666)
 	return err
 }
 
 // Retrieve will read the file and return the data
-func (r *FileStorage) Retrieve(ctx context.Context) ([]byte, error) {
-	content, err := ioutil.ReadFile(r.FilePath)
+func (r *FileStorage) Retrieve(ctx context.Context, pr, sha, filename string) ([]byte, error) {
+	address := filepath.Join(r.BasePath, pr, sha, filename)
+	content, err := ioutil.ReadFile(address)
 	if err != nil {
 		return nil, err
 	}
@@ -40,30 +66,50 @@ func (r *FileStorage) GetBasePath() string {
 	return r.BasePath
 }
 
-func (r *FileStorage) SetStoragePath(s string) string {
-	r.FilePath = s
-	return s
+
+func (r *FileStorage) List(ctx context.Context)([]meta, error) {
+	base := r.GetBasePath()
+	ans := make([]meta, 0)
+	err := filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
+		m := meta{PrNumber: path}
+
+		err = filepath.Walk(filepath.Join(base, path), func(path string, info fs.FileInfo, err error) error {
+			if !info.IsDir() {
+				return nil
+			}
+			m.CommitSHA = path
+			ans = append(ans, m.DeepCopy())
+			// TODO parsing meta data
+			return nil
+		})
+		return nil
+	})
+	return ans, err
+
 }
 
-func (r *FileStorage) BuildStoragePath(pr, sha string) (string, error) {
-	basePath, err := filepath.Abs(r.GetBasePath())
-	if err != nil {
-		return "", err
-	}
 
-	if _, err := os.Stat(basePath); os.IsNotExist(err) {
-		err := os.Mkdir(basePath, 0777)
-		if err != nil {
-			return "", err
-		}
+type meta struct {
+	RunID string `json:"run_id"`
+	CurrentName string `json:"current_name"`
+	RefName string `json:"ref_name"`
+	CurrentLog string `json:"current_log"`
+	RefLog string `json:"ref_log"`
+	PrNumber string `json:"pr_number"`
+	CommitSHA string `json:"commit_sha"`
+}
+
+func (m meta) DeepCopy() meta {
+	return meta {
+		RunID: m.RunID,
+		CurrentName: m.CurrentName,
+		RefName: m.RefName,
+		CurrentLog: m.CurrentLog,
+		RefLog: m.RefLog,
+		PrNumber: m.PrNumber,
+		CommitSHA: m.CommitSHA,
 	}
-	PRPath := filepath.Join(basePath, pr)
-	if _, err := os.Stat(PRPath); os.IsNotExist(err) {
-		err := os.Mkdir(PRPath, 0777)
-		if err != nil {
-			return "", err
-		}
-	}
-	CommitPath := filepath.Join(PRPath, fmt.Sprintf("%s.html", sha))
-	return CommitPath, nil
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,10 @@ require (
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/google/go-github/v35 v35.2.0
-	github.com/gorilla/mux v1.8.0
-	github.com/jnovack/flag v1.16.0 // indirect
+	github.com/jnovack/flag v1.16.0
 	github.com/rs/zerolog v1.22.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
-	gotest.tools/v3 v3.0.3
 )
