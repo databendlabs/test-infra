02d9df1b6c2af0adba8d807ec19a670457b92c91
zhihanz <zzh243402448@gmail.com>
2021-08-02 22:01:42 +0800

    add leader election for chatbot deployment

--- chatbots/cmd/main.go
+++ chatbots/cmd/main.go
@@ -4,21 +4,38 @@ import (
 	"context"
 	"datafuselabs/test-infra/chatbots/hook"
 	"datafuselabs/test-infra/chatbots/utils"
+	"github.com/google/uuid"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	client "k8s.io/client-go/kubernetes"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/jnovack/flag"
 	"github.com/rs/zerolog/log"
 )
 
+const (
+	LeassLockName      = "datafuse-chatbot-lock"
+	LeaseLockNamespace = "kube-system"
+)
+
 var (
-	GithubToken  string
-	WebhookToken string
-	Address      string
-	Region       string
-	Bucket       string
-	Endpoint     string
-	TemplateDir string
-	StaticDir string
+	GithubToken          string
+	WebhookToken         string
+	Address              string
+	Region               string
+	Bucket               string
+	Endpoint             string
+	TemplateDir          string
+	StaticDir            string
+	EnableLeaderElection bool
 )
 
 func init() {
@@ -30,7 +47,35 @@ func init() {
 	flag.StringVar(&Endpoint, "endpoint", "", "S3 storage endpoint")
 	flag.StringVar(&TemplateDir, "template-dir", "", "dashboard template dir")
 	flag.StringVar(&StaticDir, "static-dir", "", "dashboard static file dir")
+	flag.BoolVar(&EnableLeaderElection, "enable-leader-election", false, "configure leader election for k8s HA")
 
+}
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+		return cfg, nil
+	}
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+
+// Set up Kubernetes client using kubeconfig (or in-cluster config if no file provided)
+func clientSetup(kubeconfig string) (clientset *client.Clientset, err error) {
+	config, err := buildConfig(kubeconfig)
+	if err != nil {
+		return
+	}
+	clientset, err = client.NewForConfig(config)
+	return
 }
 
 func main() {
@@ -52,5 +97,74 @@ func main() {
 		StaticDir,
 	)
 	server := hook.NewServer(cfg)
-	server.Start()
+	// run local
+	if !EnableLeaderElection {
+		server.Start()
+		return
+	}
+
+	// needs to run on a k8s cluster
+	id := uuid.New().String()
+	clientSet, err := clientSetup("")
+	if err != nil {
+		log.Error().Msgf("error on k8s client setup %s", err.Error())
+		return
+	}
+	stopCh := make(chan struct{})
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      LeassLockName,
+			Namespace: LeaseLockNamespace,
+		},
+		Client: clientSet.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: id,
+		},
+	}
+	leadelectionCallback := leaderelection.LeaderCallbacks{
+		OnStartedLeading: func(ctx context.Context) {
+			// once leader elected it should taint all nodes at first to prevent race condition
+			log.Log().Msgf("Start to run server on %s", id)
+			server.Start()
+		},
+		OnStoppedLeading: func() {
+			// when leader failed, log leader failure and restart leader election
+			log.Log().Msgf("leader lost: %s", id)
+		},
+		OnNewLeader: func(identity string) {
+			// we're notified when new leader elected
+			if identity == id {
+				// I just got the lock
+				return
+			}
+			log.Log().Msgf("new leader elected: %s", identity)
+		},
+	}
+	func(stopCh <-chan struct{}) {
+		for {
+			ctx, cancel := context.WithCancel(context.Background())
+			ch := make(chan os.Signal, 1)
+			signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				<-ch
+				log.Log().Msgf("Received termination, signaling shutdown")
+				cancel()
+			}()
+			leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+				Lock:            lock,
+				ReleaseOnCancel: true,
+				LeaseDuration:   60 * time.Second,
+				RenewDeadline:   15 * time.Second,
+				RetryPeriod:     5 * time.Second,
+				Callbacks:       leadelectionCallback,
+			})
+			select {
+			case <-stopCh:
+				return
+			default:
+				cancel()
+				log.Error().Msgf("leader election lost due to exception happened")
+			}
+		}
+	}(stopCh)
 }
